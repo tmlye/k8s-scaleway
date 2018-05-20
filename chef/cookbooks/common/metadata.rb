f06f6e102124ea1e3fe7a5f4e63f00f29ad2670e
Sascha Eglau <se@saschaeglau.com>
2018-05-20 12:19:24 +0800

    add dependencies, update kitchen.yml

--- chef/cookbooks/common/metadata.rb
+++ chef/cookbooks/common/metadata.rb
@@ -5,3 +5,6 @@ license 'MIT'
 description 'Sets up the OS and installs common packages'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
+
+depends 'sudo'
+depends 'docker'
--- chef/cookbooks/common/recipes/user.rb
+++ chef/cookbooks/common/recipes/user.rb
@@ -17,10 +17,23 @@ directory "/home/#{username}/.ssh" do
     recursive true
 end
 
+key = node['user']['ssh_public_key']
+if key.nil?
+  if File.file?("/root/.ssh/authorized_keys")
+    key = ::File.open("/root/.ssh/authorized_keys").read
+  else
+    raise "No public key was specified and /root/.ssh/authorized_keys does not exist"
+  end
+end
+
 file "/home/#{username}/.ssh/authorized_keys" do
   owner username
   group username
   mode 0400
-  content ::File.open("/root/.ssh/authorized_keys").read
+  content key
   action :create
+end
+
+sudo username do
+  users username
 end
