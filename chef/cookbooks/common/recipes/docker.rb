3e36a332f897bdcac0144076f789debe4626c4ee
Sascha Eglau <se@saschaeglau.com>
2018-05-22 20:39:18 +0800

    add correct repository before docker install

--- chef/cookbooks/common/recipes/docker.rb
+++ chef/cookbooks/common/recipes/docker.rb
@@ -1,8 +1,18 @@
 #
 # Cookbook:: common
 # Recipe:: docker
+package 'apt-transport-https'
+package 'ca-certificates'
+
+execute 'add docker gpg key' do
+  command 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+end
+execute 'add docker repository' do
+  command 'add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && apt-get update'
+end
+
 docker_installation_package 'default' do
-  # Look up the version here: https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/arm64/
+  # Look up the version here: https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/armhf/
   version node['docker_version']
   action :create
   package_options %q|--force-yes -o Dpkg::Options::='--force-confold' -o Dpkg::Options::='--force-all'|
