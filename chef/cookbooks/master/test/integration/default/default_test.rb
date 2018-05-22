6cd535896d177a0d3136970db6adf3928b4719b7
Sascha Eglau <se@saschaeglau.com>
2018-05-22 13:16:52 +0800

    add more inspec tests

--- chef/cookbooks/master/test/integration/default/default_test.rb
+++ chef/cookbooks/master/test/integration/default/default_test.rb
@@ -1,18 +1,31 @@
-# # encoding: utf-8
-
 # Inspec test for recipe master::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe user('kuser') do
+  it { should exist }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe shadow.users('kuser') do
+  its('count') { should eq 1 }
+end
+
+describe sshd_config do
+  its('PermitRootLogin') { should eq('no') }
+  its('PasswordAuthentication') { should eq('no') }
+  its('PermitEmptyPasswords') { should eq('no') }
+  its('ChallengeResponseAuthentication') { should eq('no') }
+
+  its('RSAAuthentication') { should eq('yes') }
+  its('PubkeyAuthentication') { should eq('yes') }
+end
+
+describe command('docker') do
+  it { should exist }
+end
+
+describe docker.version do
+  its('Server.Version') { should cmp >= '18.03'}
+  its('Client.Version') { should cmp >= '18.03'}
 end
