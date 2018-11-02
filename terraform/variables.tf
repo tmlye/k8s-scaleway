0bf4506dc9ff92bf872da1021868243c6873ebba
Sascha Eglau <sascha.eglau@zuehlke.com>
2018-11-02 15:31:43 +0800

    update arch and various versions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "region" {
-  default     = "par1"
+  default     = "ams1"
   description = "Values: par1 ams1"
 }
 
 variable "server_type" {
-  default     = "C1"
-  description = "See: https://www.scaleway.com/virtual-cloud-servers/#anchor_starter"
+  default     = "ARM64-2GB"
+  description = "See the name column here: https://www.scaleway.com/virtual-cloud-servers/#anchor_starter"
 }
 
 variable "architecture" {
-  default     = "arm"
-  description = "Values: arm arm64 x86_64"
+  default     = "arm64"
+  description = "Values: arm64 x86_64"
 }
